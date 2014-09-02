05b340cf58e8c5c49409354ea7149442b2407823
Sanjay <sbpatoliya@yahoo.co.in>
2014-09-02 08:32:08 +0530

    fixes of task 7

--- js/services/eps-attachment-service.js
+++ js/services/eps-attachment-service.js
@@ -16,50 +16,59 @@ factory('Search', function() {
             for(var i = 0; i< myattachments.length; i++) {
                 name = myattachments[i].name;
                 name_array = name.split('/');
-                name_1 = name_array[0];
-                name_1 = name_1.replace("'s", '');
-                name_2 = name_array[1];
                 
-                name_1 = name_1.toLowerCase();
-                name_2 = name_2.toLowerCase();
+                if (name_array.length >= 3) {
                 
-                if (filter_earth_val != '' && filter_media_val != '') {
+                    name_1 = name_array[0];
+                    name_1 = name_1.replace("'s", '');
+                    name_2 = name_array[1];
                     
-                    var val = '';
-                    //console.log('11 >> ' + val);
-                    if (name_1.indexOf(filter_earth_val) > -1) {
-                        val = myattachments[i];
-                    }
+                    name_1 = name_1.toLowerCase();
+                    name_2 = name_2.toLowerCase();
                     
-                    if (val != '') {
-                        if (filter_media_val == 'all') {
-                            val = val;
-                        } else {
-                            if (name_2 == filter_media_val) {
-                                val = myattachments[i];
+                    if (filter_earth_val != '' && filter_media_val != '') {
+                        
+                        var val = '';
+                        //console.log('11 >> ' + val);
+                        if (name_1.indexOf(filter_earth_val) > -1) {
+                            val = myattachments[i];
+                        }
+                        
+                        if (val != '') {
+                            if (filter_media_val == 'all') {
+                                val = val;
                             } else {
-                                val = '';
+                                if (name_2 == filter_media_val) {
+                                    val = myattachments[i];
+                                } else {
+                                    val = '';
+                                }
                             }
                         }
-                    }
-                    if (val !== '') {
-                        tmp_attachments.push(val);
-                    }
-                } else if (filter_earth_val != '') {
-                    if (name_1.indexOf(filter_earth_val) > -1) {
-                        tmp_attachments.push(myattachments[i]);
-                    }
-                } else if (filter_media_val != '') {
-                    if (filter_media_val == 'all') {
-                        tmp_attachments.push(myattachments[i]);
-                    } else {
-                        if (name_2 == filter_media_val) {
+                        if (val !== '') {
+                            tmp_attachments.push(val);
+                        }
+                    } else if (filter_earth_val != '') {
+                        if (name_1.indexOf(filter_earth_val) > -1) {
                             tmp_attachments.push(myattachments[i]);
                         }
+                    } else if (filter_media_val != '') {
+                        if (filter_media_val == 'all') {
+                            tmp_attachments.push(myattachments[i]);
+                        } else {
+                            if (name_2 == filter_media_val) {
+                                tmp_attachments.push(myattachments[i]);
+                            }
+                        }
+                    } else {
+                        tmp_attachments.push(myattachments[i]);
                     }
-                } else {
-                    tmp_attachments.push(myattachments[i] );
+    
+                    
+                } else { //end of length check
+                    tmp_attachments.push(myattachments[i]);
                 }
+            
             }
             
             return tmp_attachments;    
